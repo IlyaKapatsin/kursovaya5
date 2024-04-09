373a9ae16ee96d57fa1f20bf34fa6c96a52ff42d
Kapatsin Ilya <70647559+ilya3113@users.noreply.github.com>
2024-04-09 17:44:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,49 @@
-from Class.db_manager import DBManager
-from utils.utils import create_database, create_tables, insert_data_into_tables
+from src.config import config
+from src.api import HeadHunterAPI
+from src.DBManager import DBManager
 
 
-def user_interaction():
+def new_work():
+	print('Я выбрал интересные мне компании и хочу получить данные о вакансиях по API.')
+	response = HeadHunterAPI()
+	database_name = response.database_name  # Имя базы данных
+	employers_dict = response.employers_dict  # Словарь компаний
+	employers_all_vacancies = response.get_vacancies()  # Список вакансий
+	params = config()
+	DBManager.create_database(params, database_name)
+	DBManager.create_table_employers(params, database_name)
+	DBManager.create_table_vacancy(params, database_name)
+	DBManager.add_employer_in_bd(params, database_name, employers_dict)
+	DBManager.add_vacancy_in_bd(params, database_name, employers_all_vacancies)
 
-    list_employers = [1942330, 49357, 78638, 2748, 3529, 9498112, 93787326, 2180, 1648566, 1942336]
-    dbm = DBManager()
-    create_database('db_name')
-    create_tables('db_name')
-    insert_data_into_tables(list_employers)
+	while True:
+		user_input = input("""Выберете команду:
+Нажмите 1 чтоб получить список всех компаний и количество их вакансий.
+Нажмите 2 чтоб получить список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию.
+Нажмите 3 чтоб получить среднюю зарплату по вакансиям.
+Нажмите 4 чтоб получить список всех вакансий, у которых зарплата выше средней по всем вакансиям.
+Нажмите 5 чтоб получить список всех вакансий, в названии которых содержатся переданные в метод слова, например python.
+Другие команды закончат программу.\n""")
+		if user_input not in ['1', '2', '3', '4', '5']:
+			print('Программа завершена.')
+			break
+		elif user_input == '1':
+			print(DBManager.get_companies_and_vacancies_count(params, database_name))
+			continue
+		elif user_input == '2':
+			print(DBManager.get_all_vacancies(params, database_name))
+			continue
+		elif user_input == '3':
+			print(DBManager.get_avg_salary(params, database_name))
+			continue
+		elif user_input == '4':
+			print(DBManager.get_vacancies_with_higher_salary(params, database_name))
+			continue
+		elif user_input == '5':
+			word = input('Введите искомое слово.\n')
+			print(DBManager.get_vacancies_with_keyword(params, database_name, word))
+			continue
 
-    while True:
 
-        new = input(
-            '1 - список всех компаний и кол-во вакансий у каждой компании\n'
-            '2 - список всех вакансий с указанием названия компании, вакансии, зарплаты и ссылки на вакансию\n'
-            '3 - средняя зарплата по вакансиям\n'
-            '4 - список всех вакансий, у которых зарплата выше средней по всем вакансиям\n'
-            '5 - список вакансий, в названии которых содержится ключевое слово\n'
-            'Выход - закончить\n'
-        )
-
-        if new == '1':
-            print(dbm.get_companies_and_vacancies_count())
-        elif new == '2':
-            print(dbm.get_all_vacancies())
-        elif new == '3':
-            print(dbm.get_avg_salary())
-        elif new == '4':
-            print(dbm.get_vacancies_with_higher_salary())
-        elif new == '5':
-            keyword = str(input('Найти: '))
-            print(dbm.get_vacancies_with_keyword(keyword))
-        elif new == 'Выход':
-            break
-
-if __name__ == "__main__":
-    user_interaction()
+if __name__ == '__main__':
+	new_work()
