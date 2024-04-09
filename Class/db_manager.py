95ce1b6efc2b8a03dd4ec3f4a5aea2ee3a5459bd
Kapatsin Ilya <70647559+ilya3113@users.noreply.github.com>
2024-04-09 17:47:44 +0300

    Update db_manager.py

--- Class/db_manager.py
+++ Class/db_manager.py
@@ -1,52 +1,121 @@
 import psycopg2
-from utils.config import config
 
 
 class DBManager:
-    def __init__(self, db_name):
-        self.db_name = db_name
 
-    def execute_query(self, query) -> list:
-        conn = psycopg2.connect(dbname='db_name', **config())
-        with conn:
-            with conn.cursor() as cur:
-                cur.execute(query)
-                result = cur.fetchall()
-        conn.close()
-        return result
+	@staticmethod
+	def create_database(params: dict, database_name):
+		""" Создает базу данных. """
+		conn = psycopg2.connect(dbname='postgres', **params)
+		conn.autocommit = True
+		cur = conn.cursor()
+		cur.execute(f'DROP DATABASE IF EXISTS {database_name}')
+		cur.execute(f'CREATE DATABASE {database_name}')
+		conn.close()
 
-    def get_companies_and_vacancies_count(self):
+	@staticmethod
+	def create_table_employers(params: dict, database_name):
+		""" Создает таблицу компаний. """
+		conn = psycopg2.connect(dbname=database_name, **params)
+		with conn.cursor() as cur:
+			cur.execute("""
+			CREATE TABLE employers (
+			company_id SERIAL PRIMARY KEY,
+			employer_name VARCHAR UNIQUE
+			)
+			""")
+		conn.commit()
+		conn.close()
 
-        ''' Метод, получающий список всех компаний и вакансий у каждой компании. '''
+	@staticmethod
+	def create_table_vacancy(params: dict, database_name):
+		""" Создает таблицу вакансий. """
+		conn = psycopg2.connect(dbname=database_name, **params)
+		with conn.cursor() as cur:
+			cur.execute("""
+			CREATE TABLE vacancies (
+			vacancy_id serial,
+			vacancy_name text not null,
+			salary int,
+			company_name text REFERENCES employers(employer_name) NOT NULL,
+			vacancy_url varchar not null,
+			foreign key(company_name) references employers(employer_name)
+			)
+			""")
+		conn.commit()
+		conn.close()
 
+	@staticmethod
+	def add_employer_in_bd(params: dict, database_name: str, employers_dict: dict):
+		"""Скрипт для заполнения данными компаний таблицы в БД. """
+		conn = psycopg2.connect(dbname=database_name, **params)
+		cur = conn.cursor()
+		for employer in employers_dict:
+			cur.execute(
+				f"INSERT INTO employers (employer_name) VALUES ('{employer}')")
+		conn.commit()
+		conn.close()
 
-        result = self.execute_query('SELECT employers.name, COUNT(vacancies.employer_id) AS vacancies_count FROM employers LEFT JOIN vacancies ON employers.employer_id = vacancies.employer_id GROUP BY employers.name')
-        return result
+	@staticmethod
+	def add_vacancy_in_bd(params: dict, database_name: str, employers_all_vacancies: dict):
+		"""Скрипт для заполнения данными вакансий таблицы в БД."""
+		conn = psycopg2.connect(dbname=database_name, **params)
+		cur = conn.cursor()
+		for vacancy in employers_all_vacancies:
+			cur.execute(
+				f"INSERT INTO vacancies(vacancy_name, salary, company_name, vacancy_url) values "
+				f"('{vacancy['vacancy_name']}', '{int(vacancy['salary'])}', "
+				f"'{vacancy['employer']}', '{vacancy['url']}')")
+		conn.commit()
+		conn.close()
 
+	@staticmethod
+	def get_companies_and_vacancies_count(params, database_name):
+		""" Получает список всех компаний и количество вакансий у каждой компании. """
+		with psycopg2.connect(dbname=database_name, **params)as conn:
+			with conn.cursor()as cur:
+				cur.execute('SELECT company_name, COUNT(vacancy_name) from vacancies GROUP BY company_name')
+				answer = cur.fetchall()
+		conn.close()
+		return answer
 
-    def get_all_vacancies(self):
-        ''' Метод, получающий список всех вакансий. '''
+	@staticmethod
+	def get_all_vacancies(params, database_name):
+		""" Получает список всех вакансий с указанием названия компании,
+		названия вакансии и зарплаты и ссылки на вакансию. """
+		with psycopg2.connect(dbname=database_name, **params) as conn:
+			with conn.cursor() as cur:
+				cur.execute('SELECT * FROM  vacancies')
+				answer = cur.fetchall()
+		conn.close()
+		return answer
 
-        result = self.execute_query('SELECT employers.name, vacancies.name, vacancies.salary_from, vacancies.salary_to, vacancies.url FROM employers JOIN vacancies using (employer_id)')
-        return result
+	@staticmethod
+	def get_avg_salary(params, database_name):
+		""" Получает среднюю зарплату по вакансиям. """
+		with psycopg2.connect(dbname=database_name, **params) as conn:
+			with conn.cursor() as cur:
+				cur.execute('SELECT AVG(salary) FROM  vacancies')
+				answer = cur.fetchall()
+		conn.close()
+		return answer
 
+	@staticmethod
+	def get_vacancies_with_higher_salary(params, database_name):
+		""" Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям. """
+		with psycopg2.connect(dbname=database_name, **params) as conn:
+			with conn.cursor() as cur:
+				cur.execute('SELECT * FROM  vacancies WHERE salary > (SELECT AVG(salary) FROM vacancies)')
+				answer = cur.fetchall()
+		conn.close()
+		return answer
 
-    def get_avg_salary(self):
-        ''' Метод, получающий среднюю зарплату по вакансиям. '''
-
-        result = self.execute_query('SELECT AVG(salary_from) AS payment_avg FROM vacancies')
-        return result
-
-
-    def get_vacancies_with_higher_salary(self):
-        ''' Метод, получающий список всех вакансий, у которых зарплата выше средней по всем вакансиям. '''
-
-        result = self.execute_query('SELECT * FROM vacancies WHERE salary_from > (select AVG(salary_from) FROM vacancies)')
-        return result
-
-
-    def get_vacancies_with_keyword(self, keywords):
-        ''' Метод, поиска всех вакансий по ключевому слову. '''
-
-        result = self.execute_query(f'SELECT * FROM vacancies WHERE name LIKE \'%{keywords}%\'')
-        return result
+	@staticmethod
+	def get_vacancies_with_keyword(params, database_name, word):
+		"""  Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python. """
+		with psycopg2.connect(dbname=database_name, **params) as conn:
+			with conn.cursor() as cur:
+				cur.execute(f"SELECT * FROM  vacancies WHERE vacancy_name LIKE '%{word}%'")
+				answer = cur.fetchall()
+		conn.close()
+		return answer
